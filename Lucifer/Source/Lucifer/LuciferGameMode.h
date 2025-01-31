1154b45c8df08c4ed9a508ab0539cdc01ffee8d0
hamellen <zzodong0309@naver.com>
2025-02-01 00:54:44 +0900

    ini

---
+++ Lucifer/Source/Lucifer/LuciferGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "LuciferGameMode.generated.h"
+
+UCLASS(minimalapi)
+class ALuciferGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	ALuciferGameMode();
+};
+
+
+
